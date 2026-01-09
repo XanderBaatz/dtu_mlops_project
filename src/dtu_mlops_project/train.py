5bd92234b4cf5d7f461b9742c782a48bed215fcd
Xander Baatz <35949906+XanderBaatz@users.noreply.github.com>
2026-01-09 10:04:50 +0100

    upload cookiecutter template files

---
+++ src/dtu_mlops_project/train.py
@@ -0,0 +1,10 @@
+from dtu_mlops_project.model import Model
+from dtu_mlops_project.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
