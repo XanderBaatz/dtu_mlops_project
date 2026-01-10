a987c1d2d3805259bf865500349ab3c2253f463a
Xander Baatz <35949906+XanderBaatz@users.noreply.github.com>
2026-01-10 18:00:00 +0000

    added initial dataset with print options

--- src/dtu_mlops_project/data.py
+++ src/dtu_mlops_project/data.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import typer
 from torch.utils.data import Dataset
 
+# https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/datasets/reddit.html#Reddit
+from torch_geometric.datasets import Planetoid
+#from torch_geometric.data import Dataset, download_url
 
 class MyDataset(Dataset):
     """My custom dataset."""
@@ -19,11 +22,25 @@ class MyDataset(Dataset):
     def preprocess(self, output_folder: Path) -> None:
         """Preprocess the raw data and save it to the output folder."""
 
-def preprocess(data_path: Path, output_folder: Path) -> None:
+
+def preprocess(data_path: Path = Path("data/raw"), output_folder: Path = Path("data/processed")) -> None:
     print("Preprocessing data...")
-    dataset = MyDataset(data_path)
-    dataset.preprocess(output_folder)
+    data_raw = "data"
+    dataset = Planetoid(
+        root=data_raw,
+        name="Cora",
+    )
+    #dataset = MyDataset(data_path)
+    dataset.process()
 
 
 if __name__ == "__main__":
-    typer.run(preprocess)
+    #typer.run(preprocess)
+    dataset = Planetoid(root="data", name="Cora")
+
+    print(f"Dataset: {dataset}")
+    print(f"Number of graphs: {len(dataset)}")
+    print(f"Number of nodes: {dataset[0].num_nodes}")
+    print(f"Number of edges: {dataset[0].num_edges}")
+    print(f"Node features: {dataset[0].num_node_features}")
+    print(f"Classes: {dataset.num_classes}")
