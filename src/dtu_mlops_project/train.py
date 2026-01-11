301c6498eeddaaa543f80e23cbfe7d8e05bb1fc6
Xander Baatz <35949906+XanderBaatz@users.noreply.github.com>
2026-01-11 16:54:09 +0000

    added CLI support for training

--- src/dtu_mlops_project/train.py
+++ src/dtu_mlops_project/train.py
@@ -1,11 +1,15 @@
-from dtu_mlops_project.model import GCN
-#from dtu_mlops_project.data import MyDataset
+from dtu_mlops_project.model import GCN, C8SteerableCNN
+#from dtu_mlops_project.data import pcam_dataset
 from torch_geometric.datasets import Planetoid
 import torch
 import torch_geometric
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import typer
+from typing import Annotated
 plt.style.use("ggplot")
+
+app = typer.Typer()
 
 DEVICE = torch.device(
     "cuda" if torch.cuda.is_available()
@@ -13,16 +17,23 @@ DEVICE = torch.device(
     else "cpu"
 )
 
-
-def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
+@app.command()
+def train(
+        lr: float = 1e-3,
+        batch_size: int = 32,
+        epochs: int = 100,
+        output: Annotated[str, typer.Option("--output", "-o")] = "model.pth"
+    ) -> None:
     print("Training started...")
     print("Device:", DEVICE)
     print(f"{lr=}, {batch_size=}, {epochs=}")
 
+    #model = C8SteerableCNN(n_classes=2).to(DEVICE)
     model = GCN().to(DEVICE)
     dataset = Planetoid(root="data", name="Cora")
 
     train_dataloader = torch_geometric.loader.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    #train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
@@ -33,10 +44,10 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
         "train_accuracy": [],
     }
 
-    for epoch in tqdm(range(epochs), desc="Training"):
+    for epoch in range(epochs):
         model.train() # set model to training mode
 
-        for i, data in enumerate(tqdm(train_dataloader, desc=f"Epoch {epoch}", leave=False)):
+        for i, data in enumerate(train_dataloader):
             data = data.to(DEVICE)
             optimizer.zero_grad() # clear previous gradients
 
@@ -55,9 +66,28 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
             accuracy = (out[data.train_mask].argmax(dim=1) == data.y[data.train_mask]).sum().item() / data.train_mask.sum().item()
             statistics["train_accuracy"].append(accuracy)
 
+            if i % 100 == 0:
+                print(f"Epoch {epoch+1}, iter {i}, loss: {loss.item()}")
+
+    #for epoch in range(epochs):
+    #    model.train()
+    #    for i, (img, target) in enumerate(train_dataloader):
+    #        img, target = img.to(DEVICE), target.to(DEVICE)
+    #        optimizer.zero_grad()
+    #        y_pred = model(img)
+    #        loss = loss_fn(y_pred, target)
+    #        loss.backward()
+    #        optimizer.step()
+    #        statistics["train_loss"].append(loss.item())
+    #
+    #        accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
+    #        statistics["train_accuracy"].append(accuracy)
+    #
+    #        if i % 100 == 0:
+    #            print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
+
     print("Training finished.")
-    torch.save(model.state_dict(), "models/model.pth")
-    torch.save(model.state_dict(), "models/model.pth")
+    torch.save(model.state_dict(), f"models/{output}")
     fig, axs = plt.subplots(1, 2, figsize=(15, 5))
     axs[0].plot(statistics["train_loss"])
     axs[0].set_title("Train loss")
@@ -65,6 +95,5 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
     axs[1].set_title("Train accuracy")
     fig.savefig("reports/figures/training_statistics.pdf")
 
-
 if __name__ == "__main__":
-    train(epochs=200)
+    app()
