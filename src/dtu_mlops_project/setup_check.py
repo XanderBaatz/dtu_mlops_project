228263e3ad5fcfd7e4b14b2153b57ecdc1f0801f
Xander Baatz <35949906+XanderBaatz@users.noreply.github.com>
2026-01-20 15:15:39 +0000

    removed unused py file

--- src/dtu_mlops_project/setup_check.py
+++
@@ -1,6 +0,0 @@
-import torch
-import torch_geometric
-
-print(f"PyTorch version: {torch.__version__}")
-print(f"Is CUDA available: {torch.cuda.is_available()}")
-print(f"PyTorch Geometric version: {torch_geometric.__version__}")
